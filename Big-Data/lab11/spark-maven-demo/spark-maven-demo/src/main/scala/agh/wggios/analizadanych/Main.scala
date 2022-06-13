006a22f4bb83ab4a6d4f4a789748876786cc2007
TKurcaba <63873964+TKurcaba@users.noreply.github.com>
2022-06-14 00:37:26 +0200

    Add files via upload

---
+++ Big-Data/lab11/spark-maven-demo/spark-maven-demo/src/main/scala/agh/wggios/analizadanych/Main.scala
@@ -0,0 +1,20 @@
+package agh.wggios.analizadanych
+
+import agh.wggios.analizadanych.caseclass.FlightCaseClass
+import agh.wggios.analizadanych.datareader.DataReader
+import agh.wggios.analizadanych.sparksessionprovider.SparkSessionProvider
+import agh.wggios.analizadanych.transformations.Transformations
+
+object Main extends SparkSessionProvider{
+
+  def main(args: Array[String]): Unit = {
+    logger.info("Starting application")
+    import spark.implicits._
+    val df =new DataReader("2010-summary.csv").read_csv().as[FlightCaseClass]
+    df.filter(row => new Transformations().OnlyBigAirports(row)).show()
+
+    df.groupByKey(x => x.DEST_COUNTRY_NAME).reduceGroups((l, r) => new Transformations().sum2(l, r))
+      .show()
+
+  }
+}
