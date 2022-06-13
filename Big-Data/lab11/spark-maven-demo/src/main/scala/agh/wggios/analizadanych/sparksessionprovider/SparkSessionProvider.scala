aa6d85c11e48cb2982e8a46363710fcb4f290f62
TKurcaba <63873964+TKurcaba@users.noreply.github.com>
2022-06-14 00:38:53 +0200

    Delete Big-Data/lab11/spark-maven-demo directory

--- Big-Data/lab11/spark-maven-demo/src/main/scala/agh/wggios/analizadanych/sparksessionprovider/SparkSessionProvider.scala
+++
@@ -1,7 +0,0 @@
-package agh.wggios.analizadanych.sparksessionprovider
-
-import org.apache.spark.sql.SparkSession
-
-class SparkSessionProvider {
-  val spark: SparkSession = SparkSession.builder().config("spark.master", "local").getOrCreate()
-}
